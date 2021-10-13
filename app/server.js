581c456168e1c8c6810dd02ecf98f394cd3b1dd2
Deserie Murembeni <deserie.dev@gmail.com>
2021-10-13 13:56:19 +0200

    add server.js & dependencies

---
+++ app/server.js
@@ -0,0 +1,75 @@
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.svg"));
+  res.writeHead(200, {'Content-Type': 'image/svg' });
+  res.end(img, 'binary');
+});
+
+let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
+
+let mongoUrlDocker = "mongodb://admin:password@mongodb";
+
+let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+
+let databaseName = "my-db";
+
+app.post('/update-profile', function (req, res) {
+  let userObj = req.body;
+
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+    userObj['userid'] = 1;
+
+    let myquery = { userid: 1 };
+    let newvalues = { $set: userObj };
+
+    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
+      if (err) throw err;
+      client.close();
+    });
+
+  });
+  // Send response
+  res.send(userObj);
+});
+
+app.get('/get-profile', function (req, res) {
+  let response = {};
+  // Connect to the db
+  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+
+    let db = client.db(databaseName);
+
+    let myquery = { userid: 1 };
+
+    db.collection("users").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      response = result;
+      client.close();
+
+      res.send(response ? response : {});
+    });
+  });
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
